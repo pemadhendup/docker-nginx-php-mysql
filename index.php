7809955968b8b06b87003cd5b4a0bdbe28ee6a8f
pemadhendup <49856831+pemadhendup@users.noreply.github.com>
2019-04-25 17:12:07 +0600

    made changes to index file

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $username = "root";
 $password = "123456";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=database", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully"; 
