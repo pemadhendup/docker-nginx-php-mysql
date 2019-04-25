859f4d2c8cd99c14889a5dc4ec500e362193b5bd
kapilsthakkar25 <40014158+kapilsthakkar25@users.noreply.github.com>
2019-04-25 16:21:09 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php
+$servername = "db";
+$username = "root";
+$password = "123456";
 
-$database = new PDO('mysql:host=db;dbname=database', "root", "123456");
-echo "Connected to MySQL<br />";
-
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
 ?>
